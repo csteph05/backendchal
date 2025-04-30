6ab27af903956663e3a36a3c6351787376cf79c4
csteph05 <charlescabrera697@gmail.com>
2025-04-30 14:23:58 +0800

    feat: add routes

--- src/router/questionRoutes.js
+++ src/router/questionRoutes.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const questionController = require("../controller/questioncontroller");
 
-router.post;
-router.put;
-router.delete;
-router.get;
+router.post("/create", questionController.createQuestion);
 
+router.put("/update/:questionId", questionController.updateQuestion);
+
+router.delete("/delete/:questionId", questionController.deleteQuestion);
+
+router.get("/get/:questionId", questionController.getQuestion);
+
+router.get("/list", questionController.listQuestions);
+
+router.post("check-answer/:questionId", questionController.checkAnswer);
+
+module.exports = router;
